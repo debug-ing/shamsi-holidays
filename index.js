5ab4b672242d19e3467c6a9a4c5d961433a7f5c6
M4HDI <mahdicsharp0098@gmail.com>
2022-04-27 02:49:26 +0430

    finish

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
-import Holiday from './lib/holiday.js';
-//export { holiday as default}
-
-let s =Holiday.isHoliday(1,1);
-console.log(s);
-let days = Holiday.getHolidaysMonth(1);
-console.log(days);
+'use strict';
+let server = require('./lib/holiday.js')
+module.exports = server;
